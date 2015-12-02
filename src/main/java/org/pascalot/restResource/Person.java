7ea12f24403033f5c9489d2e9a9b8be485824134
Pascal Hamisu <pascal.hamisu@here.com>
2015-12-02 01:03:42 -0600

    Person resource

--- src/main/java/org/pascalot/restResource/Person.java
+++ src/main/java/org/pascalot/restResource/Person.java
@@ -1,10 +1,59 @@
 package org.pascalot.restResource;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * Created by hamisu on 12/1/15.
  */
 public class Person implements Serializable
 {
+    private String firstName, lastName, gender;
+    private Instant dateOfBirth;
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public Instant getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public void setDateOfBirth(Instant dateOfBirth)
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString(){
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", dateOfBirth="
+                + dateOfBirth + "]";
+    }
 }
